7a94154847ad85186a363e9f99554af24e275044
Jeroen van de Sanden <jvandesanden@worldia.com>
2024-05-30 09:46:47 +0200

    feat: create action to reactivate todos

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { TodoProps } from "@/app/types";
-import { completeTodo } from "@/utils/actions";
+import { completeTodo, reactivateTodo } from "@/utils/actions";
 import { useTransition } from "react";
 
 export default function Todo({ todo }: TodoProps) {
   const [isPending, startTransition] = useTransition()  
   
   return (
-      <div className={`border border-black/20 cursor-pointer ${todo.completed && 'line-through text-gray-900'}`} onClick={() => startTransition(() => completeTodo(todo.id))}>{ todo.content }</div>
+      <div className={`border border-black/20 cursor-pointer ${todo.completed && 'line-through text-gray-900'}`} onClick={() => startTransition(() => todo.completed ? reactivateTodo(todo.id): completeTodo(todo.id))}>{ todo.content }</div>
     )
   }
--- utils/actions.ts
+++ utils/actions.ts
@@ -15,6 +15,16 @@ export const completeTodo = async (id: string) => {
     revalidatePath('/todos')
 }
 
+export const reactivateTodo = async (id: string) => {
+    await db.todo.update({
+        where: {id},
+        data: {
+            completed: false,
+        }
+    })
+    revalidatePath('/todos')
+}
+
 export const newTodo = async (formData) => {
     const todo = await db.todo.create({
         data: {
